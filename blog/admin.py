faba9ad83a7ce81cff9331f9c8bb6fe02942ebbb
Maxim Starodubcev <maxim@trambroid.com>
2013-03-18 14:06:21 +0200

    Add comments

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from models import Blog
+from models import Blog, Comment
 
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'author', 'create_time', 
-                                   'front_page', 'on_top', 'deleted')
+    list_display = ('__unicode__', 'user', 'create_time', 'front_page',
+                                                    'on_top', 'deleted')
     prepopulated_fields = {'slug': ('title',)}
 
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'create_time', 'ip')
+
 admin.site.register(Blog, BlogAdmin)
+admin.site.register(Comment, CommentAdmin)
--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Blog(models.Model):
     body = models.TextField(default='')
     slug = models.SlugField(max_length=128, unique=True)
     deleted = models.BooleanField(default=False)
-    author = models.ForeignKey(User, null=True)
+    user = models.ForeignKey(User, null=True)
     front_page = models.BooleanField(default=True)
     on_top = models.BooleanField(default=False)
     create_time = models.DateTimeField(auto_now=False, auto_now_add=True)
@@ -20,3 +20,16 @@ class Blog(models.Model):
 
     def __unicode__(self):
         return u'%s' % self.title
+
+
+class Comment(models.Model):
+    title = models.CharField(max_length=64, blank=True, default='')
+    body = models.TextField()
+    user = models.ForeignKey(User, null=True)
+    create_time = models.DateTimeField(auto_now=False, auto_now_add=True)
+    edit_time = models.DateTimeField(auto_now=True, auto_now_add=False)
+    ip = models.GenericIPAddressField(default='127.0.0.1')
+    blog = models.ForeignKey(Blog)
+
+    def __unicode__(self):
+        return u'%s: %s - %s' % (self.user, self.blog, self.body[:32])
