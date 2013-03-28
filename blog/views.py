68a781f9eeac4824b3fed93bc71d187d03a7f0e7
Maxim Starodubcev <starodubcevmax@gmail.com>
2013-03-28 21:57:38 +0200

    Add comments

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,38 @@
 # -*-coding: utf8-*-
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.context_processors import csrf
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from models import Blog, Comment
+from forms import CommentForm
 
 from tag.models import ArticleTag
 
 def blog_detail(request, slug):
+    user = request.user
+
     content = get_object_or_404(Blog, slug=slug)
     comments = Comment.objects.filter(blog=content)
     contents = {'content': content, 'comments': comments}
     contents.update(csrf(request))
+
+    if request.method == 'POST' and user.is_authenticated():
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            ip = request.META['REMOTE_ADDR']
+            comment_form.save(content, user, ip)
+            return redirect(request.META['HTTP_REFERER'])
+    else:
+        comment_form = CommentForm()
+    contents['comment_form'] = comment_form
+
     return render(request, 'blog/blog_detail.html', contents)
 
 
 def blog_list(request):
     contents = Blog.objects.all().order_by('-create_time')
     if not contents:
-	return render(request, 'blog/blog_list.html', {'content': contents})
+        return render(request, 'blog/blog_list.html', {'content': contents})
     paginator = Paginator(contents, 10)
 
     page = request.GET.get('p')
