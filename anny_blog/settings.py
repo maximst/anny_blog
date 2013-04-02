3acf9a7dc11c39f075bee23036c47922651ec766
Maxim Starodubcev <starodubcevmax@gmail.com>
2013-04-02 22:12:33 +0300

    Sape

--- anny_blog/settings.py
+++ anny_blog/settings.py
@@ -140,8 +140,8 @@ INSTALLED_APPS = (
     'social_auth',
     'coffeescript',
     'sorl.thumbnail',
-    #'linkexchange_django',
-    'sape.django',
+    'linkexchange_django',
+    #'sape.django',
 
     'blog',
     'tag',
@@ -187,8 +187,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'social_auth.context_processors.social_auth_backends',
     'social_auth.context_processors.social_auth_by_type_backends',
     'social_auth.context_processors.social_auth_login_redirect',
-    #'linkexchange_django.context_processors.linkexchange',
-    'sape.django.context_processors.sape',
+    'linkexchange_django.context_processors.linkexchange',
+    #'sape.django.context_processors.sape',
 )
 CONTEXT_PROCESSORS = TEMPLATE_CONTEXT_PROCESSORS
 
@@ -239,9 +239,9 @@ VK_API_SECRET = '40uiMRels7fjeAQgypOg'
 
 COFFEESCRIPT_ROOT = autopath('static')
 LINKEXCHANGE_CONFIG = autopath('anny_blog', 'linkexchange.cfg')
-SAPE_DATABASE = autopath('sape')
-SAPE_USER = 'f485b75b9228d78ed859121ee216bfc4'
-SAPE_HOST = HOSTNAME
+#SAPE_DATABASE = autopath('sape')
+#SAPE_USER = 'f485b75b9228d78ed859121ee216bfc4'
+#SAPE_HOST = HOSTNAME
 
 THUMBNAIL_QUALITY = 80
 
