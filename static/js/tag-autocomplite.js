36a81f93477d396a90aa84fc70ceed3aee90b2ed
Maxim Starodubcev <maxim@trambroid.com>
2013-10-10 16:55:32 +0300

    Fix js

--- static/js/tag-autocomplite.js
+++ static/js/tag-autocomplite.js
@@ -1,12 +1,14 @@
-$('#id_tags').autocomplete({
-    serviceUrl: 'service/autocomplete.ashx',
-    minChars: 2,
-    delimiter: /,\s*/,
-    maxHeight: 400,
-    width: 300,
-    zIndex: 9999,
-    deferRequestBy: 100
-//     params: { country: 'Yes'},
-//     onSelect: function(data, value){ },
-//     lookup: ['January', 'February', 'March']
-});
+(function($) {
+  $('#id_tags').autocomplete({
+      serviceUrl: 'service/autocomplete.ashx',
+      minChars: 2,
+      delimiter: /,\s*/,
+      maxHeight: 400,
+      width: 300,
+      zIndex: 9999,
+      deferRequestBy: 100
+  //     params: { country: 'Yes'},
+  //     onSelect: function(data, value){ },
+  //     lookup: ['January', 'February', 'March']
+  });
+})(django.jQuery.noConflict());
