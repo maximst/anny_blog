9651e9c3e4a646f50ca5bb7d47092c056a3fe09f
Maxim Starodubcev <starodubcevmax@gmail.com>
2013-10-10 18:27:42 +0300

    Fix

--- blog/admin.py
+++ blog/admin.py
@@ -12,11 +12,13 @@ class BlogAdmin(admin.ModelAdmin):
                                                     'on_top', 'deleted')
     prepopulated_fields = {'slug': ('title',)}
     inlines = (BlogImageInline,)
-    
+
     class Media:
         from django.conf import settings
         static_url = getattr(settings, 'STATIC_URL', '/static')
-        js = [static_url + 'js/jquery.autocomplete.js', static_url + 'js/tag-autocomplite.js']
+        js = [static_url + 'js/jquery.autocomplete.js',
+              static_url + 'js/tag-autocomplite.js',
+              static_url + 'js/jquery.js']
 
 
 class CommentAdmin(admin.ModelAdmin):
--- static/js/tag-autocomplite.js
+++ static/js/tag-autocomplite.js
@@ -1,14 +1,14 @@
 (function($) {
-  $('#id_tags').autocomplete({
-      serviceUrl: '/tag_autocomplite',
+  $(document).ready(function(){
+    var $searchBox = $('#id_tags');
+    $searchBox.autocomplete({
+      serviceUrl: '/tag_autocomplite/',
       minChars: 2,
       delimiter: /(,)\s*/,
       maxHeight: 400,
       width: 300,
       zIndex: 9999,
       deferRequestBy: 100
-  //     params: { country: 'Yes'},
-  //     onSelect: function(data, value){ },
-  //     lookup: ['January', 'February', 'March']
+    });
   });
 })(django.jQuery.noConflict());
