2c923464455bef1580ad1d08c8832cb94cd7b41c
Maxim Starodubcev <maxim@trambroid.com>
2013-10-15 16:59:42 +0300

    Get files

--- radio/models.py
+++ radio/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.conf import settings
+import os
 
 
 class Audio(models.Model):
+    DIR = 'radio'
+
     aid = models.IntegerField(unique=True)
     artist = models.CharField(max_length=128, blank=True, default='Unknown')
     title = models.CharField(max_length=128, blank=True, default='No title')
@@ -9,4 +13,11 @@ class Audio(models.Model):
     genre = models.IntegerField(blank=True, null=True)
     duration = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
-    file = models.FileField(upload_to='radio')
+    file = models.FileField(upload_to=DIR)
+
+    class Meta:
+        app_label = 'radio'
+
+    @classmethod
+    def file_dir(cls):
+        return os.path.join(settings.MEDIA_ROOT, cls.DIR)
--- radio/tasks.py
+++ radio/tasks.py
@@ -1,7 +1,18 @@
-from django.conf import settings
-
 import requests
 from BeautifulSoup import BeautifulSoup
+import os
+import sys
+import vkontakte
+
+add_path = '/'.join(os.path.split(os.path.abspath(__file__))[0].split('/')[:-1])
+sys.path.append(add_path)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "anny_blog.settings")
+
+#from django.core.wsgi import get_wsgi_application
+#application = get_wsgi_application()
+from django.conf import settings
+from django.core.files.temp import NamedTemporaryFile
+from radio.models import Audio
 
 
 def vk_login():
@@ -28,14 +39,38 @@ def vk_login():
             if attrs['name'] != 'submit':
                 post_data[attrs['name']] = attrs.get('value')
 
-        post_data['email'] = ''
-        post_data['pass'] = ''
+        post_data['email'] = settings.VK_EMAIL
+        post_data['pass'] = settings.VK_PASS
 
         r = requests.post(action, data=post_data)
-        return r
+        query = r.url.split('#')[1]
+        args = dict(item.split('=') for item in query.split('&'))
+
+        return args['access_token']
+
+def add_song(song):
+    tmp_file = os.path.join(Audio.file_dir(), 'tmp', song['aid'].__str__())
+    fd_mp3 = open(tmp_file, 'w+')
+    result = requests.get(song['url'])
+    fd_mp3.write(result.content)
 
 def get_audio():
-    vk_login()
+    token = vk_login()
+    vk = vkontakte.API(token=token)
+    vk_audio = vk.audio.get(gid=settings.VK_GROUP_ID)
+    vk_aids = [a['aid'] for a in vk_audio]
+    db_audio = Audio.objects.all()
+    db_aids = [a.aid for a in db_audio]
+
+    for audio in vk_audio:
+        if audio['aid'] not in db_aids:
+            add_song(audio)
+
+    # Delete removed vk songs
+    [a.delete() for a in db_audio if a.aid not in vk_aids]
+
+    from pprint import pprint
+    pprint(audio)
 
 if __name__ == '__main__':
     get_audio()
